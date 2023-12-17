0e43c69b83bd2af5369f46144774c75f041ac58d
Sefinek <contact@sefinek.net>
2023-12-17 16:52:25 +0100

    Fixes for API & add new headers

--- index.js
+++ index.js
@@ -3,44 +3,83 @@ const md5 = require('./scripts/md5.js');
 const useragent = require('./scripts/useragent.js');
 
 const USER_AGENT = useragent();
-const COOKIE_EXPIRATION_TIME = 86400000;
+const DEFAULT_HEADERS = {
+	'Accept': '*/*',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Connection': 'keep-alive',
+	'Content-Type': 'text/plain;charset=UTF-8',
+	'Host': 'www.cleverbot.com',
+	'Origin': 'https://www.cleverbot.com',
+	'Sec-Ch-Ua': '"Not_A Brand";v="8"',
+	'Sec-Ch-Ua-Mobile': '?0',
+	'Sec-Ch-Ua-Platform': '"Windows"',
+	'Sec-Fetch-Dest': 'empty',
+	'Sec-Fetch-Mode': 'cors',
+	'Sec-Fetch-Site': 'same-origin',
+	'Sec-Gpc': '1',
+	'User-Agent': USER_AGENT,
+};
+const COOKIE_EXPIRATION_TIME = 86400000; // 24 hours
 const MAX_RETRY_ATTEMPTS = 15;
 let cbsId, cookies, lastCookieUpdate = 0, lastResponse, xai;
 
 async function updateCookiesIfNeeded() {
-	if (!cookies || Date.now() - lastCookieUpdate >= COOKIE_EXPIRATION_TIME) {
-		const cookieResponse = await axios.get(`https://www.cleverbot.com/extras/conversation-social-min.js?${new Date().toJSON().split('T')[0].split('T')[0]}`, {
-			headers: { 'User-Agent': USER_AGENT },
-		});
+	try {
+		if (!cookies || Date.now() - lastCookieUpdate >= COOKIE_EXPIRATION_TIME) {
+			const cookieResponse = await axios.get(`https://www.cleverbot.com/extras/conversation-social-min.js?${new Date().toJSON().split('T')[0]}`, {
+				headers: {
+					...DEFAULT_HEADERS,
+					'Cookie': cookies ? `${cookies[0].split(';')[0]}; _cbsid=-1` : '',
+				},
+			});
 
-		cookies = cookieResponse.headers['set-cookie'];
-		lastCookieUpdate = Date.now();
+			cookies = cookieResponse.headers['set-cookie'];
+			lastCookieUpdate = Date.now();
+		}
+	} catch (err) {
+		console.error(`Error updating cookies: ${err.message}`);
+		throw err;
 	}
 }
 
-function buildAPIUrl(stimulus) {
-	return `https://www.cleverbot.com/webservicemin?uc=UseOfficialCleverbotAPI${cbsId ? `&out=${encodeURIComponent(lastResponse)}&in=${encodeURIComponent(stimulus)}&bot=c&cbsid=${cbsId}&xai=${xai}&ns=2&al=&dl=&flag=&user=&mode=1&alt=0&reac=&emo=&sou=website&xed=&` : ''}`;
+function buildAPIUrl(stimulus, language) {
+	let url = 'https://www.cleverbot.com/webservicemin?uc=UseOfficialCleverbotAPI&ncf=V2';
+	if (cbsId) {
+		url += `&out=${encodeURIComponent(lastResponse)}&in=${encodeURIComponent(stimulus)}&bot=c&cbsid=${cbsId}&xai=${xai}&ns=4&dl=${language}`;
+	}
+	return url;
 }
 
 async function callCleverbotAPI(stimulus, context, language) {
-	let payload = `stimulus=${encodeURIComponent(stimulus)}&${context.slice().reverse().map((c, i) => `vText${i + 2}=${encodeURIComponent(c)}`).join('&')}`;
-	if (language) payload += `&cb_settings_language=${language}`;
-	payload += `&cb_settings_scripting=no&islearning=1&icognoid=wsf&icognocheck=${md5(payload.substring(7, 33))}`;
-
-	const response = await axios.post(buildAPIUrl(stimulus), payload, {
-		timeout: 60000,
-		headers: {
-			'Cookie': `${cookies[0].split(';')[0]}; _cbsid=-1`,
-			'User-Agent': USER_AGENT,
-			'Content-Type': 'text/plain',
-		},
+	let payload = `stimulus=${encodeURIComponent(stimulus)}`;
+	context.forEach((c, i) => {
+		payload += `&vText${i + 2}=${encodeURIComponent(c)}`;
 	});
+	payload += `&cb_settings_scripting=no&sessionid=${cbsId || ''}&islearning=1&icognoid=wsf&icognocheck=${md5(payload.substring(7, 33))}`;
 
-	const responseLines = response.data.split('\r');
-	cbsId = responseLines[1];
-	xai = `${cbsId.substring(0, 3)},${responseLines[2]}`;
-	lastResponse = responseLines[0];
-	return lastResponse;
+	try {
+		const response = await axios.post(buildAPIUrl(stimulus, language), payload, {
+			timeout: 60000,
+			headers: {
+				...DEFAULT_HEADERS,
+				'Content-Length': Buffer.byteLength(payload),
+				'Cookie': cookies ? `${cookies[0].split(';')[0]}; _cbsid=-1` : '',
+			},
+		});
+
+		const responseLines = response.data.split('\r');
+		if (responseLines.length >= 3) {
+			cbsId = responseLines[1];
+			xai = `${cbsId.substring(0, 3)},${responseLines[2]}`;
+			lastResponse = responseLines[0];
+		} else {
+			throw new Error(`Invalid response format from Cleverbot API: ${response.data}`);
+		}
+		return lastResponse;
+	} catch (err) {
+		console.error(`Error calling Cleverbot API: ${err.message}`);
+		throw err;
+	}
 }
 
 module.exports = async (stimulus, context = [], language = 'en') => {
