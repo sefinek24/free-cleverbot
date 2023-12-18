ca1e603e883e20a4bfce6d3d91df40618e151ed4
Sefinek <contact@sefinek.net>
2023-12-18 04:40:02 +0100

    Fixes

--- index.js
+++ index.js
@@ -1,73 +1,61 @@
 const axios = require('axios');
 const sleep = require('./scripts/sleep.js');
 const md5 = require('./scripts/md5.js');
-const useragent = require('./scripts/useragent.js');
-
-const DEFAULT_HEADERS = {
-	'Accept': '*/*',
-	'Accept-Encoding': 'gzip, deflate, br',
-	'Connection': 'keep-alive',
-	'Content-Type': 'text/plain;charset=UTF-8',
-	'Host': 'www.cleverbot.com',
-	'Origin': 'https://www.cleverbot.com',
-	'Sec-Ch-Ua': '"Not_A Brand";v="8"',
-	'Sec-Ch-Ua-Mobile': '?0',
-	'Sec-Fetch-Dest': 'empty',
-	'Sec-Fetch-Mode': 'cors',
-	'Sec-Fetch-Site': 'same-origin',
-	'Sec-Gpc': '1',
-	'User-Agent': useragent(),
-};
+const DEFAULT_HEADERS = require('./scripts/get-useragent.js');
 
 const MAX_RETRY_ATTEMPTS = 3;
-const RETRY_BASE_COOLDOWN = 4000; // Base cooldown period in milliseconds (4 seconds)
-const COOKIE_EXPIRATION_TIME = 86400000; // 24 hours
-let cbsId, cookies, lastCookieUpdate = 0, lastResponse, xai;
+const RETRY_BASE_COOLDOWN = 4000;
+const COOKIE_EXPIRATION_TIME = 15768000; // 4,38 hours
+
+let cookies;
+let lastCookieUpdate = 0;
+
+let cbsId;
+let xai;
+let ns = 0;
+let lastResponse;
 
 async function updateCookiesIfNeeded() {
-	try {
-		if (!cookies || Date.now() - lastCookieUpdate >= COOKIE_EXPIRATION_TIME) {
-			const cookieResponse = await axios.get(`https://www.cleverbot.com/extras/conversation-social-min.js?${new Date().toJSON().split('T')[0]}`, {
+	if (!cookies || Date.now() - lastCookieUpdate >= COOKIE_EXPIRATION_TIME) {
+		try {
+			const cookieResponse = await axios.get(`https://www.cleverbot.com/extras/conversation-social-min.js?${new Date().toISOString().split('T')[0].replace(/-/g, '')}`, {
 				timeout: 25000,
 				headers: {
 					...DEFAULT_HEADERS,
-					// 'Cookie': cookies ? `${cookies[0].split(';')[0]}; _cbsid=-1` : '',
+					'Cookie': '_cbsid=-1; note=1',
 				},
 			});
 
 			cookies = cookieResponse.headers['set-cookie'];
 			lastCookieUpdate = Date.now();
-		}
-	} catch (err) {
-		if (err.response && err.response.status === 403) {
-			throw new Error(`Failed to update cookies. Your IP is banned. ${err.message}`);
-		} else {
-			throw new Error(`Failed to update cookies. Unknown error. ${err.message}`);
+		} catch (err) {
+			throw new Error(`Failed to update cookies: ${err.message}`);
 		}
 	}
 }
 
-function buildAPIUrl(stimulus, language) {
-	let url = 'https://www.cleverbot.com/webservicemin?uc=UseOfficialCleverbotAPI&ncf=V2';
-	if (cbsId) url += `&out=${encodeURIComponent(lastResponse)}&in=${encodeURIComponent(stimulus)}&bot=c&cbsid=${cbsId}&xai=${xai}&ns=4&dl=${language}`;
-
-	return url;
+function buildPayload(stimulus, context, language) {
+	let payload = `stimulus=${encodeURIComponent(stimulus)}&`;
+	context.reverse().forEach((msg, i) => {
+		payload += `vText${i + 2}=${encodeURIComponent(msg)}&`;
+	});
+	payload += `${language ? `cb_settings_language=${language}&` : ''}cb_settings_scripting=no&islearning=1&icognoid=wsf&icognocheck=`;
+	return payload + md5(payload.substring(7, 33));
 }
 
 async function callCleverbotAPI(stimulus, context, language) {
-	let payload = `stimulus=${encodeURIComponent(stimulus)}`;
-	context.forEach((c, i) => {
-		payload += `&vText${i + 2}=${encodeURIComponent(c)}`;
-	});
-	payload += `&cb_settings_scripting=no&sessionid=${cbsId || ''}&islearning=1&icognoid=wsf&icognocheck=${md5(payload.substring(7, 33))}`;
+	await updateCookiesIfNeeded();
+	const payload = buildPayload(stimulus, context, language);
 
 	try {
-		const response = await axios.post(buildAPIUrl(stimulus, language), payload, {
+		ns = ns + 1;
+		const url = `https://www.cleverbot.com/webservicemin?uc=UseOfficialCleverbotAPI&ncf=V2&${cbsId ? `out=${encodeURIComponent(lastResponse)}&in=${encodeURIComponent(stimulus)}&bot=c&cbsid=${cbsId}&xai=${xai}&ns=${ns}&al=&dl=&flag=&user=&mode=1&alt=0&reac=&emo=&sou=website&xed=&` : ''}`;
+		const response = await axios.post(url, payload, {
 			timeout: 19000,
 			headers: {
 				...DEFAULT_HEADERS,
 				'Content-Length': Buffer.byteLength(payload),
-				'Cookie': cookies ? `${cookies[0].split(';')[0]}; _cbsid=-1` : '',
+				'Cookie': cookies ? `_cbsid=-1; note=1; ${cookies[0].split(';')[0]}; ${xai ? `XAI=${cbsId.substring(0, 3)};` : ''} ${lastResponse ? `CBALT=1~${lastResponse};` : ''}` : '',
 			},
 		});
 
@@ -76,34 +64,28 @@ async function callCleverbotAPI(stimulus, context, language) {
 			cbsId = responseLines[1];
 			xai = `${cbsId.substring(0, 3)},${responseLines[2]}`;
 			lastResponse = responseLines[0];
-		} else {
-			throw new Error(`Invalid response format from Cleverbot API: ${response.data}`);
+			return lastResponse;
 		}
-		return lastResponse;
+
+		throw new Error('Invalid response format from Cleverbot API');
 	} catch (err) {
-		throw new Error(`Cleverbot API call failed. ${err.message}`);
+		throw new Error(`Cleverbot API call failed: ${err.message}`);
 	}
 }
 
 module.exports = async (stimulus, context = [], language = 'en') => {
-	await updateCookiesIfNeeded();
-
 	for (let i = 0; i < MAX_RETRY_ATTEMPTS; i++) {
 		try {
 			return await callCleverbotAPI(stimulus, context, language);
 		} catch (err) {
 			if (err.response && err.response.status === 403) {
-				throw new Error(`Error on attempt ${i + 1}. Sorry, but your IP address is banned. ${err.message}`);
+				throw new Error(`Attempt ${i + 1} failed: IP address is banned. ${err.message}`);
 			} else {
-				// Random additional cooldown between 1 and 5 seconds
-				const additionalCooldown = Math.floor(Math.random() * 4000) + 1000;
-				const waitTime = RETRY_BASE_COOLDOWN + additionalCooldown;
-
-				console.log(`Error on attempt ${i + 1}. ${err.message}. Waiting ${waitTime / 1000}s...`);
+				const waitTime = RETRY_BASE_COOLDOWN + Math.floor(Math.random() * 4000) + 1000;
+				console.log(`Attempt ${i + 1} failed: ${err.message}. Waiting ${waitTime / 1000}s...`);
 				await sleep(waitTime);
 			}
 		}
 	}
-
-	throw new Error(`Failed to get a response from Cleverbot after ${MAX_RETRY_ATTEMPTS} attempts. ${cbsId ? `Session ${cbsId}.` : 'Unknown session.'}`);
+	throw new Error(`Failed to get a response from Cleverbot after ${MAX_RETRY_ATTEMPTS} attempts.`);
 };
---
+++ scripts/get-useragent.js
@@ -0,0 +1,32 @@
+const randomUserAgent = require('./random-useragent.js');
+const { userAgent, secChUa, secChUaFullArch, secChUaFullBitness, secChUaFullVersion, secChUaFullVersionList, secChUaPlatform, secChUaPlatformVersion } = randomUserAgent();
+
+const DEFAULT_HEADERS = {
+	'Accept': '*/*',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'en;q=0.7',
+	'Cache-Control': 'no-cache',
+	'Connection': 'keep-alive',
+	'Content-Type': 'text/plain;charset=UTF-8',
+	'Host': 'www.cleverbot.com',
+	'Pragma': 'no-cache',
+	'Origin': 'https://www.cleverbot.com',
+	'Referer': 'https://www.cleverbot.com/',
+	'Sec-Ch-Ua': secChUa,
+	'Sec-Ch-Ua-Arch': secChUaFullArch,
+	'Sec-Ch-Ua-Bitness': secChUaFullBitness,
+	'Sec-Ch-Ua-Full-Version': secChUaFullVersion,
+	'Sec-Ch-Ua-Full-Version-List': secChUaFullVersionList,
+	'Sec-Ch-Ua-Mobile': '?0',
+	'Sec-Ch-Ua-Model': '""',
+	'Sec-Ch-Ua-Platform': secChUaPlatform,
+	'Sec-Ch-Ua-Platform-Version': secChUaPlatformVersion,
+	'Sec-Ch-Ua-Wow64': '?0',
+	'Sec-Fetch-Dest': 'empty',
+	'Sec-Fetch-Mode': 'cors',
+	'Sec-Fetch-Site': 'same-origin',
+	'Sec-Gpc': '1',
+	'User-Agent': userAgent,
+};
+
+module.exports = DEFAULT_HEADERS;
