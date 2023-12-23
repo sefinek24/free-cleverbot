5e77efccab5985f4514119e2a5a6cf3ea354cd72
Sefinek <contact@sefinek.net>
2023-12-24 00:11:22 +0100

    New types (:

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,145 @@
 declare module 'free-cleverbot' {
-    type Stimulus = string;
-    type Context = string[] | undefined;
-    type Language =
+    export type Stimulus = string;
+    export type Context = string[];
+    export type Language =
         'af' | 'id' | 'ms' | 'ca' | 'cs' | 'da' | 'de' | 'en' | 'es' | 'eu' |
         'ti' | 'fr' | 'gl' | 'hr' | 'zu' | 'is' | 'it' | 'lt' | 'hu' | 'nl' |
         'no' | 'pl' | 'pt' | 'ro' | 'sl' | 'fi' | 'sv' | 'vi' | 'tr' | 'el' |
-        'bg' | 'ru' | 'sr' | 'uk' | 'ko' | 'zh' | 'ja' | 'hi' | 'th' | undefined;
+        'bg' | 'ru' | 'sr' | 'uk' | 'ko' | 'zh' | 'ja' | 'hi' | 'th';
+
+    type Headers = {
+        'Accept': string;
+        'Accept-Encoding': string;
+        'Accept-Language': string;
+        'Cache-Control': string;
+        'Connection': string;
+        'Content-Type': string;
+        'Host': string;
+        'Pragma': string;
+        'Origin': string;
+        'Referer': string;
+        'Sec-Ch-Ua': string;
+        'Sec-Ch-Ua-Arch': string;
+        'Sec-Ch-Ua-Bitness': string;
+        'Sec-Ch-Ua-Full-Version': string;
+        'Sec-Ch-Ua-Full-Version-List': string;
+        'Sec-Ch-Ua-Mobile': string;
+        'Sec-Ch-Ua-Model': string;
+        'Sec-Ch-Ua-Platform': string;
+        'Sec-Ch-Ua-Platform-Version': string;
+        'Sec-Ch-Ua-Wow64': string;
+        'Sec-Fetch-Dest': string;
+        'Sec-Fetch-Mode': string;
+        'Sec-Fetch-Site': string;
+        'Sec-Gpc': string;
+        'User-Agent': string;
+    };
+
+    export interface Config {
+        debug?: boolean;
+        defaultLanguage?: Language;
+        maxRetryAttempts?: number;
+        retryBaseCooldown?: number;
+        cookieExpirationTime?: number;
+    }
+
+    export interface CookieData {
+        content: string[] | undefined;
+        lastUpdate: number;
+    }
+
+    export interface SessionData {
+        cbsId: string | undefined;
+        xai: string | undefined;
+        ns: number;
+        lastResponse: string | undefined;
+    }
+
+    export interface RequestData {
+        successfulRequestsCount: number;
+        failedRequestsCount: number;
+        headers: Headers;
+    }
+
+    export interface CleverBotData {
+        debug: boolean;
+        selectedLanguage: Language;
+        maxRetryAttempts: number;
+        retryBaseCooldown: number;
+        cookie: CookieData;
+        session: SessionData;
+        request: RequestData;
+    }
+
 
     /**
-     * The main function of the module, which communicates with the Cleverbot API.
+     * The main function of the module, communicating with the Cleverbot API.
      * @param stimulus - The input text for Cleverbot.
-     * @param context - Optional conversation context as an array of strings.
+     * @param context - The required conversation context as an array of strings.
      * @param language - Optional language code for the Cleverbot session.
      * @returns The response from Cleverbot as a string.
+     * @example
+     * const CleverBot = require('free-cleverbot');
+     *
+     * CleverBot.config({
+     *    debug: false,
+     *    defaultLanguage: 'en',
+     *    maxRetryAttempts: 5,
+     *    retryBaseCooldown: 4000,
+     *    cookieExpirationTime: 18000000,
+     * });
+     *
+     * const msg = 'Hello';
+     * const context = [];
+     *
+     * (async () => {
+     *    try {
+     *        const res = await CleverBot.interact(msg, context);
+     *
+     *        context.push(msg);
+     *        context.push(res);
+     *
+     *        console.log(res);
+     *    } catch (err) {
+     *        console.error('Sorry, but something went wrong ):', err);
+     *    }
+     * })();
      * @throws {Error} If it fails to get a response after the maximum number of attempts.
      */
-    function CleverBot(stimulus: Stimulus, context?: Context, language?: Language): Promise<string>;
+    export function interact(stimulus: Stimulus, context: Context, language?: Language): Promise<string>;
 
-    export = CleverBot;
+    /**
+     * Function responsible for configuring the module.
+     * Allows setting various configuration options for the Cleverbot module.
+     *
+     * @param config - The configuration object containing settings.
+     * @example
+     * CleverBot.config({
+     *    debug: false,
+     *    defaultLanguage: 'en',
+     *    maxRetryAttempts: 5,
+     *    retryBaseCooldown: 4000,
+     *    cookieExpirationTime: 18000000,
+     * });
+     * @throws {Error} If the provided configuration object is invalid.
+     */
+    export function config(config: Config): void;
+
+    /**
+     * Returns the current session data stored in RAM and other information.
+     *
+     * @example console.log(CleverBot.getData());
+     * @returns An object with Cleverbot data.
+     */
+    export function getData(): CleverBotData;
+
+    /**
+     * Represents the version number of the `free-cleverbot` module.
+     * This property contains a string that specifies the current version of the module,
+     * conforming to the Semantic Versioning (SemVer) standard.
+     *
+     * @example console.log(CleverBot.version); // Displays e.g. '2.0.0'
+     * @return The current version of the module.
+     */
+    export const version: string;
 }
