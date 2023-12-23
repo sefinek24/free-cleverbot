4b66dde4cc6643cf87cb0f73bf43ffac332359bb
Sefinek <contact@sefinek.net>
2023-12-23 23:09:54 +0100

    Fixes

--- scripts/languages.js
+++ scripts/languages.js
@@ -1,8 +1,6 @@
-const SUPPORTED_LANGUAGES = new Set([
+module.exports = new Set([
 	'af', 'id', 'ms', 'ca', 'cs', 'da', 'de', 'en', 'es', 'eu', 'ti', 'fr',
 	'gl', 'hr', 'zu', 'is', 'it', 'lt', 'hu', 'nl', 'no', 'pl', 'pt', 'ro',
 	'sl', 'fi', 'sv', 'vi', 'tr', 'el', 'bg', 'ru', 'sr', 'uk', 'ko', 'zh',
 	'ja', 'hi', 'th',
 ]);
-
-module.exports = SUPPORTED_LANGUAGES;
