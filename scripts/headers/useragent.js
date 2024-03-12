03c517ad9a4708cf46b9015ad53c7dc97deaa1b0
Sefinek Actions <contact@sefinek.net>
2024-03-13 00:45:34 +0100

    Update useragents

--- scripts/headers/useragent.js
+++ scripts/headers/useragent.js
@@ -1,19 +1,19 @@
 const userAgents = [
 	// Windows - Chrome
 	{
-		agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-		secChUa: '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+		agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+		secChUa: '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
 		secChUaFullArch: '"x86"',
 		secChUaFullBitness: '"64"',
 		secChUaFullVersion: '"120.0.6099.110"',
-		secChUaFullVersionList: '"Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.110", "Google Chrome";v="120.0.6099.110"',
+		secChUaFullVersionList: '"Not_A Brand";v="8.0.0.0", "Chromium";v="122.0.6261.129", "Google Chrome";v="122.0.6261.129"',
 		secChUaPlatform: '"Windows"',
 		secChUaPlatformVersion: '"10.0.0"',
 	},
 
 	// Windows - Firefox
 	{
-		agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0',
+		agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0',
 	},
 
 	// MacOS - Chrome
